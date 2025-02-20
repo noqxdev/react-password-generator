9735ec546288bfc39b0a84affa13a04236533efc
noqx <noel.merz09@outlook.com>
2025-02-20 14:57:30 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Slider, { sliderClasses } from '@mui/material/Slider';
+import Slider from '@mui/material/Slider';
 import Button from '@mui/material/Button';
 import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
 import Grow from '@mui/material/Grow';
 import { SnackbarProvider, useSnackbar } from 'notistack';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import passwordGenerator from './passwordGenerator';
 
 function App() {
   const [checked, setChecked] = useState(true);
   const { enqueueSnackbar } = useSnackbar();
-   const [sliderValue, setSliderValue] = useState(sliderValue);
+  const [sliderValue, setSliderValue] = useState(25);
 
   const handleClick = () => {
     enqueueSnackbar('Copied to clipboard!', { variant: 'success' });
@@ -19,6 +20,10 @@ function App() {
 
   const handleClickVariant = (variant) => () => {
     enqueueSnackbar('This is a success message!', { variant });
+  };
+
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
   };
 
   return (
@@ -31,10 +36,15 @@ function App() {
         >
           <div>
             <img src={logo} className="App-logo" alt="logo" />
-            
-
             <h1>React PasswordGenerator</h1>
-            <Slider className="custom-slider" defaultValue={25} aria-label="Default" valueLabelDisplay="auto" value={sliderValue} />
+            <Slider
+              className="custom-slider"
+              defaultValue={25}
+              aria-label="Default"
+              valueLabelDisplay="auto"
+              value={sliderValue}
+              onChange={handleSliderChange}
+            />
             <Button variant="contained" startIcon={<ContentCopyRoundedIcon />} onClick={handleClick}>
               Copy
             </Button>
